89ab3fe666f6fa1f561db7a80968482968ad0d01
Spartacus <k_morter@outlook.com>
2020-05-26 09:10:09 -0600

    added dates feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,8 @@ var createTask = function(taskText, taskDate, taskList) {
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
+  //check due date
+  auditTask(taskLi);
 
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
@@ -76,12 +78,20 @@ $('.list-group').on('click', 'span', function() {
   var dateInput = $('<input>').attr("type", "text").addClass('form-control').val(date);
   //swap out elements
   $(this).replaceWith(dateInput);
+  //enable jquery ui datepicker
+  dateInput.datepicker({
+    minDate: 1,
+    onClose: function(){
+      //when calendar is closed, force a "change" event on the 'dateInput'
+      $(this).trigger('change');
+    }
+  });
   //automatically focus on new element
   dateInput.trigger('focus');
 });
 
 //value of due date was changed
-$('.list-group').on('blur', "input[type='text']", function(){
+$('.list-group').on('change', "input[type='text']", function(){
   //get current text
   var date = $(this).val().trim();
   //get the parents ul's id attribute
@@ -94,6 +104,8 @@ $('.list-group').on('blur', "input[type='text']", function(){
   //recreate span element with bootstrap classes
   var taskSpan = $('<span>').addClass('badge badge-primary badge-pill').text(date);
   $(this).replaceWith(taskSpan);
+  // Pass task's <li> element into auditTask() to check new due date
+  auditTask($(taskSpan).closest(".list-group-item"));
 });
 
 
@@ -139,9 +151,6 @@ $("#remove-tasks").on("click", function() {
   }
   saveTasks();
 });
-
-// load tasks for the first time
-loadTasks();
 
 //sortable function
 $(".card .list-group").sortable({
@@ -200,5 +209,31 @@ $("#trash").droppable({
     console.log("out");
   }
 })
+
+$('#modalDueDate').datepicker({
+  minDate: 1
+});
+
+var auditTask = function(taskEl) {
+  //get the date from the task element
+  var date = $(taskEl).find('span').text().trim();
+
+  //covert to moment object at 5:00pm
+  var time = moment(date, "L").set('hour', 17);
+
+  //remove any old classes from element
+  $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+
+  //apply new class if task is near/over due date
+  if (moment().isAfter(time)) {
+    $(taskEl).addClass("list-group-item-danger");
+  } 
+  else if (Math.abs(moment().diff(time, "days")) <= 2) {
+    $(taskEl).addClass("list-group-item-warning");
+  }
+};
+
+// load tasks for the first time
+loadTasks();
 
 
