cdc178f5e8b79bcb7b251f8a28654ca50c437165
Spartacus <k_morter@outlook.com>
2020-05-27 14:07:17 -0600

    ux enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -122,7 +122,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -158,10 +158,21 @@ $(".card .list-group").sortable({
   scroll: false,
   tolerance: "pointer",
   helper: "clone",
-  activate: function(event){},
-  deactivate: function(event) {},
-  over: function(event) {},
-  out: function(event) {},
+  activate: function(event){
+    $(this).addClass('dropover');
+    $('.bottom-trash').addClass('bottom-trash-drag');
+  },
+  deactivate: function(event) {
+    $(this).removeClass('dropover');
+    $('.bottom-trash').removeClass('bottom-trash-drag');
+  },
+  over: function(event) {
+    $(event.target).addClass('dropover-active');
+  },
+  out: function(event) {
+    $(event.target).removeClass('dropover-active');
+    $('.bottom-trash').removeClass('bottom-trash-active');
+  },
   update: function(event) {
     // array to store the task data in
     var tempArr = [];
@@ -201,12 +212,13 @@ $("#trash").droppable({
   tolerance: "touch",
   drop: function(event, ui) {
     ui.draggable.remove();
+    $('.bottom-trash').removeClass('bottom-trash-active');
   },
   over: function(event, ui) {
-    console.log("over");
+    $('.bottom-trash').addClass('bottom-trash-active');
   },
   out: function(event, ui) {
-    console.log("out");
+    $('.bottom-trash').removeClass('bottom-trash-active');
   }
 })
 
@@ -232,6 +244,12 @@ var auditTask = function(taskEl) {
     $(taskEl).addClass("list-group-item-warning");
   }
 };
+
+setInterval(function(){
+  $(".card .list-group-item").each(function (el){
+    auditTask(el);
+  })
+}, 1800000);
 
 // load tasks for the first time
 loadTasks();
